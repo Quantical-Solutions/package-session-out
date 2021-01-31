4d823778735b6e97da6bd3ba5c52612bcd2d67a5
Fred-QS <66546741+Fred-QS@users.noreply.github.com>
2021-01-31 23:02:49 +0100

    Update modal.blade.php

--- src/views/modal.blade.php
+++ src/views/modal.blade.php
@@ -1,9 +1,22 @@
 <div id="modal-quantic">
-    <label class="modal-quantic__bg" onclick="closeSessionOutModal();"></label>
-    <div class="modal-quantic__inner">
-        <label class="modal-quantic__close" onclick="closeSessionOutModal();"></label>
-        <h2>SESSION EXPIRED!</h2>
-        <p>Your session has expired,</p>
-        <p>to resume your current activity please login again in another tab</p>
+    <div class="bg-white shadow sm:rounded-lg">
+        <div class="px-4 py-5 sm:p-6">
+            <h3 class="text-lg leading-6 font-medium text-gray-900">
+                {{ __('Session expiration...') }}
+            </h3>
+            <div class="mt-2 sm:flex sm:items-start sm:justify-between">
+                <div class="max-w-xl text-sm text-gray-500">
+                    <p>
+                        {{ __('Your session has expired') }},<br>{{ __('to resume your current activity please click on the
+                        button below') }}.
+                    </p>
+                </div>
+                <div class="mt-5 sm:mt-0 sm:ml-6 sm:flex-shrink-0 sm:flex sm:items-center">
+                    <button type="button" class="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" onclick="closeSessionOutModal();">
+                        {{ __('Going on') }}
+                    </button>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
