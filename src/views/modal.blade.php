e5b25d227b3e6641a458e410ce7c9cf6bd0ca359
Fred-QS <66546741+Fred-QS@users.noreply.github.com>
2021-01-31 23:16:05 +0100

    Update modal.blade.php

--- src/views/modal.blade.php
+++ src/views/modal.blade.php
@@ -1,14 +1,16 @@
-<div id="modal-quantic">
+<div id="modal-quantic" class="flex justify-center items-center">
     <div class="bg-white shadow sm:rounded-lg">
         <div class="px-4 py-5 sm:p-6">
             <h3 class="text-lg leading-6 font-medium text-gray-900">
-                {{ __('Session expiration...') }}
+                {{ __('Session expiration') }}...
             </h3>
             <div class="mt-2 sm:flex sm:items-start sm:justify-between">
                 <div class="max-w-xl text-sm text-gray-500">
                     <p>
-                        {{ __('Your session has expired') }},<br>{{ __('to resume your current activity please click on the
-                        button below') }}.
+                        {{ __('Your session has expire') }}.
+                    </p>
+                    <p>
+                        {{ __('To resume your current activity please click on the button') }}.
                     </p>
                 </div>
                 <div class="mt-5 sm:mt-0 sm:ml-6 sm:flex-shrink-0 sm:flex sm:items-center">
