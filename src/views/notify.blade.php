ba4d0e126c84b074b0ab9b8b58b111f9bff8c0d4
Fred-QS <66546741+Fred-QS@users.noreply.github.com>
2021-01-31 10:27:21 +0100

    init

---
+++ src/views/notify.blade.php
@@ -0,0 +1,20 @@
+@auth
+    <link rel="stylesheet" href="{{ asset('vendor/sessionout/css/session-modal.css') }}" />
+
+    {{-- Modal --}}
+    @include('vendor.sessionout.modal')
+
+    <script type="text/javascript">
+        window.sessionout = window.sessionout || {};
+        sessionout.authpingEndpoint = "{{ route('sessionout.chkauth') }}";
+        sessionout.requestGap = {{ config('expiredsession.gap_seconds') }};
+        sessionout.userId = {{ auth()->user()->id }};
+        sessionout.usingBroadcasting = {{ config('expiredsession.avail_broadcasting') === true? 1 : 0 }};
+    </script>
+    <script type="text/javascript" src="{{ asset('vendor/sessionout/dist/js/main.js') }}"></script>
+    <script type="text/javascript">
+        function closeSessionOutModal(){
+            document.getElementById("modal-devsrv").style.visibility = "hidden";
+        }
+    </script>
+@endauth
